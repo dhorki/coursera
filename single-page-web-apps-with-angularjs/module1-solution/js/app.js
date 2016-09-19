ce36d97d65ca0331eedbcd5b6ae08489b76f3580
vmendez1 <vmendez1@lenovo.com>
2016-09-19 14:40:37 -0500

    module 1 solution

---
+++ single-page-web-apps-with-angularjs/module1-solution/js/app.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunchCheck = function() {
+    if ($scope.list != undefined) {
+      var arrayList;
+      arrayList = $scope.list.split(",");
+
+      var count = 0;
+
+      for (var i = 0; i < arrayList.length; i++) {
+        if (arrayList[i].length > 0) {
+          count++;
+        }//if
+      }//for
+      var dif = 0;
+      dif = arrayList.length - count;
+
+      var difMessage = ""
+      difMessage = dif > 0 ? " (" + dif + " empty items not counted)" : "";
+
+      if (count <= 3) {
+        $scope.message = "Enjoy!" + difMessage;
+        $scope.format = "green";
+      } else {
+        $scope.message = "Too much!" + difMessage;
+        $scope.format = "green";
+      }//else
+    } else {
+      $scope.message = "Please enter data first";
+      $scope.format = "red";
+    }//else
+  }
+}
+
+})();
