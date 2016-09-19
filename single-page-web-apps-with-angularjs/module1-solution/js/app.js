b956b95880cc7982dd148fcc8fe0d85d3545c0b3
vmendez1 <vmendez1@lenovo.com>
2016-09-19 15:06:10 -0500

    improved message logic with empty (blanks) items

--- single-page-web-apps-with-angularjs/module1-solution/js/app.js
+++ single-page-web-apps-with-angularjs/module1-solution/js/app.js
@@ -7,35 +7,32 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.lunchCheck = function() {
+    var count = 0;
+    var difMessage = "";
     if ($scope.list != undefined) {
       var arrayList;
       arrayList = $scope.list.split(",");
 
-      var count = 0;
-
       for (var i = 0; i < arrayList.length; i++) {
-        if (arrayList[i].length > 0) {
+        if (arrayList[i].trim().length > 0) {
           count++;
         }//if
       }//for
       var dif = 0;
       dif = arrayList.length - count;
 
-      var difMessage = ""
       difMessage = dif > 0 ? " (" + dif + " empty items not counted)" : "";
-      if (count == 0) {
-        $scope.message = "Please enter data first" + difMessage;
-        $scope.format = "red";
-      } else if (count <= 3) {
-        $scope.message = "Enjoy!" + difMessage;
-        $scope.format = "green";
-      } else {
-        $scope.message = "Too much!" + difMessage;
-        $scope.format = "green";
-      }//else
-    } else {
-      $scope.message = "Please enter data first";
+
+    }
+    if (count == 0) {
+      $scope.message = "Please enter data first" + difMessage;
       $scope.format = "red";
+    } else if (count <= 3) {
+      $scope.message = "Enjoy!" + difMessage;
+      $scope.format = "green";
+    } else {
+      $scope.message = "Too much!" + difMessage;
+      $scope.format = "green";
     }//else
   }
 }
